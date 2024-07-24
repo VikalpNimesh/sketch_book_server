f622119201d49190b71ba8a27ebfa5b96db080b1
vikalp <vikalpnimesh786@gmail.com>
2024-07-24 15:54:15 +0530

    server file added

---
+++ index.js
@@ -0,0 +1,34 @@
+const express = require("express");
+const { createServer } = require("node:http");
+const { join } = require("node:path");
+const { Server } = require("socket.io");
+const cors = require("cors");
+const { url } = require("node:inspector");
+const app = express();
+
+const isDev = app.settings.env === "development"
+const url = isDev ? "http://localhost:3000" : "https://sketch-book-plum.vercel.app"
+app.use(cors({ origin: url }));
+
+const server = createServer(app);
+
+const io = new Server(server, { cors: url });
+
+io.on("connection", (socket) => {
+  socket.emit("hello", "wewe");
+
+  socket.on("drawPath", (arg) => {
+    socket.broadcast.emit("drawPath", arg);
+  });
+  socket.on("beginPath", (arg) => {
+    socket.broadcast.emit("beginPath", arg);
+  });
+  socket.on("changeConfig", (arg) => {
+    console.log(arg);
+    socket.broadcast.emit("changeConfig", arg);
+  });
+});
+
+server.listen(5000, () => {
+  console.log("server running at http://localhost:5000");
+});
